1e7b117581e9a6c1bba11802ffc198f257e25bc7
Matheus Costa <matheuscostap@gmail.com>
2020-11-10 21:18:33 -0300

    Adicionado navigation

---
+++ app/src/main/java/com/costa/matheus/filmesapi/view/settings/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package com.costa.matheus.filmesapi.view.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.costa.matheus.filmesapi.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+}
