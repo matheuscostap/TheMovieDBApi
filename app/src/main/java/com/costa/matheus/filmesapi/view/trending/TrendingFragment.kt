53dc3c4cee72ae5d0e018b093a69f427f2a1f790
Matheus Costa <matheuscostap@gmail.com>
2020-11-17 21:02:06 -0300

    Lista de filmes trending

--- app/src/main/java/com/costa/matheus/filmesapi/view/trending/TrendingFragment.kt
+++ app/src/main/java/com/costa/matheus/filmesapi/view/trending/TrendingFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.costa.matheus.filmesapi.R
+import com.costa.matheus.filmesapi.model.dto.MovieModel
 import com.costa.matheus.filmesapi.repository.state.RequestState
+import kotlinx.android.synthetic.main.fragment_trending.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -16,6 +20,8 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class TrendingFragment : Fragment() {
 
     private val viewModel: TrendingViewModel by viewModel()
+    private lateinit var adapter: TrendingListAdapter
+    private var movies = arrayListOf<MovieModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +33,23 @@ class TrendingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_trending, container, false)
+        return view
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupList()
         observeViewModel()
         viewModel.getTrending()
-        return inflater.inflate(R.layout.fragment_trending, container, false)
+    }
+
+    private fun setupList() {
+        adapter = TrendingListAdapter(requireContext(), movies)
+        val llm = LinearLayoutManager(requireContext())
+        rv_trending.adapter = adapter
+        rv_trending.layoutManager = llm
     }
 
     private fun observeViewModel() {
@@ -39,17 +58,29 @@ class TrendingFragment : Fragment() {
 
                 when(state){
                     is RequestState.Loading -> {
-                        Log.i("Trending", "Loading")
+                        trending_progress_bar.visibility = View.VISIBLE
                     }
 
                     is RequestState.Success -> {
+                        trending_progress_bar.visibility = View.GONE
                         Log.i("Trending", state.data.toString())
+
+                        state.data?.let { response ->
+                            movies.addAll(response.results)
+                            adapter.notifyDataSetChanged()
+                        }
                     }
 
                     is RequestState.Error -> {
+                        trending_progress_bar.visibility = View.GONE
                         if(!state.consumed) {
                             Log.i("Trending", state.throwable.message)
                             state.consumed = true
+
+                            Toast.makeText(
+                                requireContext(),
+                                "Erro: ${state.throwable.message}",
+                                Toast.LENGTH_LONG).show()
                         }
                     }
                 }
---
+++ app/src/main/java/com/costa/matheus/filmesapi/view/trending/TrendingListAdapter.kt
@@ -0,0 +1,39 @@
+package com.costa.matheus.filmesapi.view.trending
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.costa.matheus.filmesapi.R
+import com.costa.matheus.filmesapi.model.dto.MovieModel
+import kotlinx.android.synthetic.main.trending_movie_row.view.*
+
+class TrendingListAdapter (
+    val ctx: Context,
+    private val items: List<MovieModel>): RecyclerView.Adapter<TrendingListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendingListViewHolder {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.trending_movie_row, parent, false)
+        return TrendingListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrendingListViewHolder, position: Int) {
+        val movie = items[position]
+
+        holder.tv_movie_name.text = movie.title
+        val path = "https://image.tmdb.org/t/p/w200"
+        holder.iv_movie_poster.load("$path${movie.backdrop_path}")
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+}
+
+class TrendingListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val tv_movie_name = itemView.tv_movie_name
+    val iv_movie_poster = itemView.iv_movie_poster
+}
